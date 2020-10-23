e1075222f67b87a06dc2421c0ae5a3dc328bf2d9
Sasha Garan <sasagaran1@gmail.com>
2020-10-23 20:11:39 +0300

    Add content and Fancy Scroll

--- js/script.js
+++ js/script.js
@@ -26,3 +26,26 @@ themeButton.onclick = function() {
 	footr.classList.toggle('bg-muted');
 	footr.classList.toggle('text-dark');
 }
+
+ScrollReveal({ reset: true });
+ScrollReveal({ easing: 'cubic-bezier(0.17,0.84,0.44,1)' });
+ScrollReveal({ rotate: { x: -60, y: 0, z: 0 } });
+ScrollReveal({ scale: 0.92 });
+ScrollReveal({ viewFactor: 0.1 });
+
+
+ScrollReveal().reveal('.card-tag', { delay: 220 });
+
+ScrollReveal().reveal('.smooth_scroll', { delay: 40 });
+ScrollReveal().reveal('.smooth_scroll', { interval: 80 });
+
+ScrollReveal().reveal('.smooth_out_container', { delay: 100 });
+ScrollReveal().reveal('.smooth_in_container', { delay: 120 });
+
+ScrollReveal().reveal('table', { delay: 220 });
+ScrollReveal().reveal('tr', { delay: 420 });
+ScrollReveal().reveal('tr', { interval: 100 });
+
+// $(document).ready(function() {
+// 	$(".toast").toast('show');
+// });
