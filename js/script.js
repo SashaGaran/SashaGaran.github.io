8bea9ce4da2c416486ca676bc20a312b647c8416
Sasha Garan <sasagaran1@gmail.com>
2020-10-31 15:53:14 +0200

    Added content && optimize

--- js/script.js
+++ js/script.js
@@ -27,21 +27,20 @@ themeButton.onclick = function() {
 	footr.classList.toggle('text-dark');
 }
 
-// ScrollReveal({ reset: true });
+ScrollReveal({ reset: true });
 ScrollReveal({ easing: 'cubic-bezier(0.17,0.84,0.44,1)' });
 ScrollReveal({ rotate: { x: -60, y: 0, z: 0 } });
 ScrollReveal({ scale: 0.92 });
 ScrollReveal({ viewFactor: 0.2 });
+ScrollReveal({ opacity: 0.0 });
 
 
-ScrollReveal().reveal('.card-tag', { delay: 220 });
+ScrollReveal().reveal('.card-tag', { delay: 220, origin: 'top', scale: 1.0, distance: '10px', rotate: { x: 80, y: 0, z: 0}});
 
-ScrollReveal().reveal('.smooth_scroll', { delay: 40 });
-ScrollReveal().reveal('.smooth_scroll', { interval: 80 });
+ScrollReveal().reveal('.smooth_scroll', { delay: 40, interval: 80 });
 
 ScrollReveal().reveal('.smooth_out_container', { delay: 100 });
 ScrollReveal().reveal('.smooth_in_container', { delay: 120 });
 
 ScrollReveal().reveal('table', { delay: 220 });
-ScrollReveal().reveal('tr', { delay: 420 });
-ScrollReveal().reveal('tr', { interval: 100 });
+ScrollReveal().reveal('tr', { delay: 420, interval: 100 });
